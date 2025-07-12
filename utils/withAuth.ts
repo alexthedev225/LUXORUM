e335e06f8b86beca4899eab83be50554999a5a5f
alexthedev225 <alexthedev225@github.com>
2025-07-12 00:52:30 +0000

    fix error

--- utils/withAuth.ts
+++ utils/withAuth.ts
@@ -2,22 +2,20 @@ import {
   getUserFromRequest,
   UserPayload,
 } from "@/middleware/getUserFromRequest";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function withAuth(
-  req: Request,
-  handler: (req: Request, user: UserPayload) => Promise<Response>
+  req: NextRequest,
+  handler: (req: NextRequest, user: UserPayload) => Promise<Response>
 ): Promise<Response> {
-  const userOrResponse = await getUserFromRequest(req as any);
+  const userOrResponse = await getUserFromRequest(req);
 
   if (
     userOrResponse instanceof NextResponse &&
     (userOrResponse.status === 401 || userOrResponse.status === 403)
   ) {
-    // Renvoyer la réponse 401/403 directement si pas authentifié
     return userOrResponse;
   }
 
-  // user est bien présent, on appelle le handler avec l'user injecté
   return handler(req, (userOrResponse as { user: UserPayload }).user);
 }
